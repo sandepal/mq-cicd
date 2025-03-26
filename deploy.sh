2f60391ba99dc80ab78023a54fc33fc0445cf29e
alex <psandip@gmail.com>
2025-03-27 01:33:59 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -83,10 +83,10 @@ oc secrets link pipeline-admin $secret_name --for=mount -n $ci_namespace
 
 cat pipeline/cicd-storage.yaml_template |
        sed "s#{{DEFAULT_FILE_STORAGE}}#$file_storage#g;" |
-       sed "s#{{NAMESPACE}}#$ci_namespace#g;" > cicd-storage$ci_namespace.yaml
+       sed "s#{{CI_NAMESPACE}}#$ci_namespace#g;" > cicd-storage$ci_namespace.yaml
 
 oc apply -f cicd-storage$ci_namespace.yaml
-rm cicd-storage$ci_namespace.yaml
+#rm cicd-storage$ci_namespace.yaml
 
 cat pipeline/cicd-initialise.yaml_template |
        sed "s#{{SRCREPOS}}#$srcrepos#g;" |
@@ -97,6 +97,6 @@ cat pipeline/cicd-initialise.yaml_template |
        sed "s#{{QMGR_NAME_1}}#$qmgr_name_1#g;" |
        sed "s#{{QMGR_NAME_2}}#$qmgr_name_2#g;" > cicd-initialise$ci_namespace.yaml
 oc apply -f cicd-initialise$ci_namespace.yaml
-rm cicd-initialise$ci_namespace.yaml
+#rm cicd-initialise$ci_namespace.yaml
 
 
