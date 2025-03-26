463c462cbfdceb7d051927798b956d13cff40b20
alex <psandip@gmail.com>
2025-03-27 01:23:55 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -8,66 +8,63 @@
 # Contract with IBM Corp.
 #******************************************************************************
 
-namespace=${1:-"cp4i"}
-file_storage=${2:-"ocs-storagecluster-cephfs"}
-BLOCK_STORAGE=${3:-"ocs-storagecluster-ceph-rbd"}
-PATCH_STORAGE=${4:-true}
+# Set Git repository and MQ details
+ci_namespace="$cp4i"
+cd_namespace="$ucqm-demo"
+qmgr_name_1="ucqm1"
+qmgr_name_2="ucqm2"
+srcrepos="https://github.com/sandepal/mq-cicd.git"
+trgtrepos="https://github.com/sandepal/mq-argocd.git"
+branch="main"
+file_storage="ocs-storagecluster-cephfs"
+block_storage="ocs-storagecluster-ceph-rbd"
 
-oc new-project $namespace
-oc project $namespace
+# Create the target namespace if it doesn't exist
+oc get ns $cd_namespace >/dev/null 2>&1 || oc new-project $cd_namespace
+oc get ns $ci_namespace >/dev/null 2>&1 || oc new-project $ci_namespace
+oc project $ci_namespace
 
-#setup/deploy.sh $namespace
-
-if [ "$PATCH_STORAGE" = true ] ; then
-  kubectl patch storageclass $file_storage -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-fi
-
-
-KEY_FILE=".tekton_git"
-SECRET_NAME="git-ssh-key"
+key_file=".tekton_git"
+secret_name="git-ssh-key"
 
 # Generate SSH key pair if not already present
-if [[ ! -f "$KEY_FILE" ]]; then
+if [[ ! -f "$key_file" ]]; then
   echo "Generating SSH key..."
-  ssh-keygen -t rsa -b 4096 -C "tekton@ci" -f "$KEY_FILE" -N ""
+  ssh-keygen -t rsa -b 4096 -C "tekton@ci" -f "$key_file" -N ""
   
   # Show public key and remind user to add it to GitHub
   echo "Copy this public key to GitHub (Settings > SSH Keys):"
   echo "------------------------------------------------------"
-  cat "${KEY_FILE}.pub"
+  cat "${key_file}.pub"
   echo "------------------------------------------------------"
   read -p "Press Enter after uploading it to GitHub..."  
 else
   echo "SSH key already exists, skipping generation."
 fi
 
-
 ssh-keyscan github.com > known_hosts
 
 # Create or update the secret in OpenShift
-oc create secret generic $SECRET_NAME \
-  --from-file=ssh-privatekey="$KEY_FILE" \
+oc create secret generic $secret_name \
+  --from-file=ssh-privatekey="$key_file" \
   --from-file=known_hosts=known_hosts \
   --type=kubernetes.io/ssh-auth \
-  -n $namespace \
+  -n $ci_namespace \
   --dry-run=client -o yaml | oc apply -f -
 
-echo "Secret '$SECRET_NAME' is ready in namespace '$namespace'"
+echo "Secret '$secret_name' is ready in namespace '$ci_namespace'"
 
-
-
-
-KEY_FILE=".ibm_entitlement_key"
+entitlement_key=".ibm_entitlement_key"
 
 # Check if file exists and is not empty
-if [[ -s "$KEY_FILE" ]]; then
-    IBM_ENTITLEMENT_KEY=$(cat "$KEY_FILE")
+if [[ -s "$entitlement_key" ]]; then
+    IBM_ENTITLEMENT_KEY=$(cat "$entitlement_key")
 else
     # Prompt user for input
     read -sp "Enter IBM Entitlement Key: " IBM_ENTITLEMENT_KEY
     echo
     # Save to file for future use
-    echo "$IBM_ENTITLEMENT_KEY" > "$KEY_FILE"
+    echo "$IBM_ENTITLEMENT_KEY" > "$entitlement_key"
 fi
 
 # Create ibm-entitlement-key
@@ -75,52 +72,31 @@ oc create secret docker-registry ibm-entitlement-key \
   --docker-server=cp.icr.io \
   --docker-username=cp \
   --docker-password="$IBM_ENTITLEMENT_KEY" \
-  -n $namespace
+  -n $ci_namespace
 
+oc create serviceaccount pipeline-admin -n $ci_namespace
+oc create clusterrolebinding cicd-pipeline-admin-crb-$ci_namespace --clusterrole=cluster-admin --serviceaccount=$ci_namespace:pipeline-admin
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$ci_namespace:pipeline-admin
 
-oc create serviceaccount pipeline-admin -n $namespace
-oc create clusterrolebinding cicd-pipeline-admin-crb-$namespace --clusterrole=cluster-admin --serviceaccount=$namespace:pipeline-admin
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:$namespace:pipeline-admin
-
-oc secrets link pipeline-admin ibm-entitlement-key --for=pull -n $namespace
-oc secrets link pipeline-admin $SECRET_NAME --for=mount -n $namespace
-
-oc apply -f pipeline/git-clone-task.yaml
-sleep 10
-
-#cat pipeline/cicd-environment-setup.yaml_template |
-#       sed "s#{{DEFAULT_FILE_STORAGE}}#$file_storage#g;" |
-#       sed "s#{{NAMESPACE}}#$namespace#g;" > cicd-environment-setup$namespace.yaml
-#oc apply -f cicd-environment-setup$namespace.yaml
-#rm cicd-environment-setup$namespace.yaml
+oc secrets link pipeline-admin ibm-entitlement-key --for=pull -n $ci_namespace
+oc secrets link pipeline-admin $secret_name --for=mount -n $ci_namespace
 
 cat pipeline/cicd-storage.yaml_template |
        sed "s#{{DEFAULT_FILE_STORAGE}}#$file_storage#g;" |
-       sed "s#{{NAMESPACE}}#$namespace#g;" > cicd-storage$namespace.yaml
+       sed "s#{{NAMESPACE}}#$ci_namespace#g;" > cicd-storage$ci_namespace.yaml
 
-oc apply -f cicd-storage$namespace.yaml
-rm cicd-storage$namespace.yaml
+oc apply -f cicd-storage$ci_namespace.yaml
+rm cicd-storage$ci_namespace.yaml
 
-export SRCREPO=https://github.com/sandepal/mq-cicd.git
-export TRGTREPO=https://github.com/sandepal/mq-argocd.git
-export BRANCH=main
-export TARGET_NAMESPACE=${1:-"cp4i"}
-export QMGR_NAME_1=ucqm1
-export QMGR_NAME_2=ucqm2
+cat pipeline/cicd-initialise.yaml_template |
+       sed "s#{{SRCREPOS}}#$srcrepos#g;" |
+       sed "s#{{TRGTREPOS}}#$trgtrepos#g;" |
+       sed "s#{{CI_NAMESPACE}}#$ci_namespace#g;" |
+	   sed "s#{{CD_NAMESPACE}}#$cd_namespace#g;" |
+       sed "s#{{BRANCH}}#$branch#g;" |
+       sed "s#{{QMGR_NAME_1}}#$qmgr_name_1#g;" |
+       sed "s#{{QMGR_NAME_2}}#$qmgr_name_2#g;" > cicd-initialise$ci_namespace.yaml
+oc apply -f cicd-initialise$ci_namespace.yaml
+rm cicd-initialise$ci_namespace.yaml
 
-cat pipeline/cicd-push.yaml_template |
-       sed "s#{{SRCREPO}}#$SRCREPO#g;" |
-	   sed "s#{{TRGTREPO}}#$TRGTREPO#g;" |
-       sed "s#{{NAMESPACE}}#$TARGET_NAMESPACE#g;" |
-       sed "s#{{BRANCH}}#$BRANCH#g;" |
-       sed "s#{{QMGR_NAME_1}}#$QMGR_NAME_1#g;" |
-       sed "s#{{QMGR_NAME_2}}#$QMGR_NAME_2#g;" > cicd-push$namespace.yaml
-oc apply -f cicd-push$namespace.yaml
-#rm cicd-push$namespace.yaml
 
-#sleep 30
-
-#URL=$( oc get routes -n $namespace el-environment-setup-pipeline-trigger-route -o jsonpath={.spec.host})
-#echo {\"namespace\": \"$namespace\"} >> JSON$namespace
-#curl -d @JSON$namespace http://$URL
-#rm JSON$namespace
