6251bf1635981225d4d7885fb4376fd0da21eea2
alex <psandip@gmail.com>
2025-03-27 11:13:19 +0800

    update

---
+++ pipeline/installmqPipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#******************************************************************************
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2023. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+#******************************************************************************
+
+echo "Hello from task"
